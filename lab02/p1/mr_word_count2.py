eb9b74d69e3076b617b60012b621416df047f33e
Shunyang Li <shunyangli0@gmail.com>
2021-09-27 20:58:56 +1000

    fix mapper

--- lab02/p1/mr_word_count2.py
+++ lab02/p1/mr_word_count2.py
@@ -3,15 +3,19 @@ from mrjob.job import MRJob
 
 class WordCount(MRJob):
 
+    def mapper_init(self):
+        self.tmp = {}
+
     def mapper(self, _, line):
-        tmp = {}
         words = re.split("[ *$&#/\t\n\f\"\'\\,.:;?!\[\](){}<>~\-_]", line.lower())
         for word in words:
             if len(word):
-                tmp[word] = tmp.get(word, 0) + 1
-        
-        for k, v in tmp.items():
+                self.tmp[word] = self.tmp.get(word, 0) + 1
+    
+    def mapper_final(self):
+        for k, v in self.tmp.items():
             yield (k, v)
+
     
     def reducer(self, key, values):
         yield key, sum(values)
